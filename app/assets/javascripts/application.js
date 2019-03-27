8a4cc854d9c9706a955b0fde9ab7f76ac14e864e
山田章善 <ax261.v@gmail.com>
2019-03-27 22:25:04 +0900

    マイページお気にりViewの修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,33 @@
 //= require activestorage
 //= require jquery
 //= require bootstrap-sprockets
- //= require_tree .
+//= require_tree .
 
 
-
+$(function () {
+    $('.slider').slick({
+        
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+})
